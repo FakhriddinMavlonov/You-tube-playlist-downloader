920e9959296ca0d98389f7f619299f8e6c9a5f56
FakhriddinMavlonov <mavlonovfahriddin0822@gmail.com>
2026-03-29 06:53:29 +0500

    docker removed and binar local edded

--- start.sh
+++ start.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-
-# 1. Telegram Bot API serverini yuklab olish
-echo "📥 Telegram Bot API server yuklanmoqda..."
-# Linux 64-bit uchun mos keladigan versiya
-curl -L "https://github.com/tdlib/telegram-bot-api/releases/download/v6.3/telegram-bot-api-linux-amd64" -o telegram-bot-api
-
-chmod +x telegram-bot-api
 mkdir -p ./tg-data
+chmod +x ./telegram-bot-api
 
-# 2. Local Serverni ishga tushirish
 echo "🚀 Local Server ishga tushmoqda..."
-./telegram-bot-api \
-    --api-id=33279940 \
-    --api-hash=ebec3314c624540f0591ec66eb021ca0 \
-    --local \
-    --http-port=8081 \
-    --dir=./tg-data &
+./telegram-bot-api --api-id=33279940 --api-hash=ebec3314c624540f0591ec66eb021ca0 --local --http-port=8081 --dir=./tg-data &
 
-# 15 soniya kutamiz
 sleep 15
 
-# 3. Botni ishga tushirish
 echo "🤖 Bot ishga tushmoqda..."
 node bot.js
